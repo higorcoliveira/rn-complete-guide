268af14171310a5ff52ff3f35275c5a98dfb5a4e
higoroliveira <higor.oliveira@afferolab.com.br>
2019-08-22 19:45:11 -0300

    App guessing a number (outro branch)

--- App.js
+++ App.js
@@ -1,55 +1,20 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, Button } from 'react-native';
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
+import { StyleSheet, View, Button } from 'react-native';
+import Header from './components/Header';
+import StartGame from './screens/StartGame'
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [isAddMode, setIsAddMode] = useState(false);
   
-  const addGoalHandler = goal => {
-    setCourseGoals(currentGoals => [
-      ...currentGoals, 
-      { id: Math.random().toString(), value: goal }
-    ]);
-    setIsAddMode(false);
-  }
-
-  const removeGoalHandler = goalId => {
-    setCourseGoals(currentGoals => {
-        return currentGoals.filter((goal) => goal.id !== goalId);
-    });
-  }
-
-  const cancelGoalAdditionHandler = () => {
-    setIsAddMode(false);
-  }
-
   return (
-    <View style={styles.container}>
-      <Button title="Add New Goal" onPress={() => setIsAddMode(true)}/>
-      <GoalInput 
-        visible={isAddMode} 
-        addGoalHandler={addGoalHandler} 
-        onCancel={cancelGoalAdditionHandler}
-      />
-      <FlatList 
-        keyExtractor={(item, index) => item.id}
-        data={courseGoals}
-        renderItem={itemData => (
-          <GoalItem
-            id={itemData.item.id}
-            onDelete={removeGoalHandler} 
-            title={itemData.item.value}
-          />
-        )}
-      />
+    <View style={styles.screen}>
+        <Header title="Guess a Number" />
+        <StartGame />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 50
-  }
+    screen: {
+      flex: 1
+    }
 });
---
+++ constants/Colors.js
@@ -0,0 +1,4 @@
+export default {
+    primary: '#339cff',
+    secondary: '#c717fc'
+};
