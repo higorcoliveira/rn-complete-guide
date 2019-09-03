879ae6e65899a466fc08329f44dcf5b404d1cba7
higorcoliveira <higorcoliveira@gmail.com>
2019-09-03 20:15:58 -0300

    Finalizando lógica do jogo

--- App.js
+++ App.js
@@ -3,18 +3,36 @@ import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
 import StartGame from './screens/StartGame';
 import Game from './screens/Game';
+import GameOver from './screens/GameOver';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);    
+  }
   
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  }
+
+  const gameOverHandler = (numRounds) => {
+    setGuessRounds(numRounds);
   }
 
   let content = <StartGame onStartGame={startGameHandler} />;
 
-  if (userNumber) {
-    content = <Game userChoice={userNumber} />;
+  if (userNumber && guessRounds <= 0) {
+    content = <Game userChoice={userNumber} onGameOver={gameOverHandler} />;
+  } else if (guessRounds > 0) {
+    content = <GameOver 
+                rounds={guessRounds} 
+                userNumber={userNumber} 
+                onRestart={configureNewGameHandler}
+              />;
   }
 
   return (
